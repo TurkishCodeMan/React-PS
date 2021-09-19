9f7164092a3106f40d7b750d7500dddfcf2c20b4
Huseyin Altikulaç <huseyinaltikulac4@gmail.com>
2021-09-19 21:55:28 +0300

    -

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { getWidthAsPercentOfTotalWidth } from "../../utils/percentUtils";
-
+import React from "react";
 function ProgressBar({ percent, width, height }) {
 
     function getColor(percent) {
